0129ee467971816d638349d2eabfdd3b0e832a4d
Anna <anamacam.am@gmail.com>
2022-01-10 22:25:25 -0500

    #DB
    Realizo el script de la db

--- database/db.sql
+++ database/db.sql
@@ -1,12 +1,24 @@
 
 
-CREATE TABLE IF NOT EXISTS cuentas(
-     code INT NOT NULL,
-     name CHAR(50),
-     dni FLOAT,
-     numero_cuetal FLOAT
-     PRIMARY KEY('code')
+CREATE TABLE IF NOT EXISTS personas(
+    id = Column(Integer, primary_key=True),
+    nombre = Column(String(100), nullable=False),
+    fecha_nacimiento = Column(DateTime, nullable=False),
+    dni = Column(String(8), nullable=False),
+    ciudad = Column(String(100), nullable=False),
+    monto = Column(Integer, primary_key=True),
+    id_cuentas dni NULL
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS proceso_cuentas(
+CREATE TABLE IF NOT EXISTS cuentas(
 )
+    dni = Column(String(8), nullable=False)
+    depositos = Column(String(100), nullable=False)
+    gastos = Column(DateTime, nullable=False)
+    transferencias = Column(String(8), nullable=False)
+    monto = Column(Integer, primary_key=True)
+    PRIMARY KEY(dni)
+);
+
+ALTER TABLE personas ADD FOREIGN KEY(id_cuentas) REFERENCES cuentas(dni);
